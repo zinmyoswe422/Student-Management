3fa8695d20a9178e93c9d318e2f62e0c2e8d9697
zinmyoswe422 <zinmyoswe@cityparking.app>
2024-10-30 15:52:42 +0700

    Add project files.

---
+++ StudentPortal.Web/Models/AddStudentViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentPortal.Web.Models
+{
+    public class AddStudentViewModel
+    {
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String Phone { get; set; }
+        public bool Subscribed { get; set; }
+    }
+}
---
+++ StudentPortal.Web/Models/Entities/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentPortal.Web.Models.Entities
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String Phone { get; set; }
+        public bool Subscribed { get; set; }
+
+    }
+}
